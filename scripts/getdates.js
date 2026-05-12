4d913fd8bbeb99749c62ff655a65733af644505f
mirland242 <tsatymirland@gmail.com>
2026-05-12 01:34:29 +0000

    change

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+/*Getting the currenyt date */
+
+document.getElementById("currentyear").textContent= new Date().getFullYear();
+
+/*updating the last modified time*/
+
+document.getElementById("lastModified").innerHTML= document.lastModified;
