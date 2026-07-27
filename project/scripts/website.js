123c0311885d8555e37fc304d7000de68025230f
mirland242 <tsatymirland@gmail.com>
2026-07-27 14:37:12 +0000

    check it up

--- project/scripts/website.js
+++ project/scripts/website.js
@@ -7,51 +7,20 @@ button.addEventListener("click", () => {
 });
 
 
-// const fixedNave = document.querySelector('.navigation');
-// window.addEventListener('scroll', ()=>{
-//     if(scrollY>200){
-//         fixedNave.style.top="-150px";
-        
-//     }
-//      if(scrollY<40){
-//         fixedNave.style.top="0";
-        
-//     }
+//scrolling event
 
-// })
-
-//Scroll Evennt------------//
-// const scroll= document.querySelector('.header');
-// window.addEventListener('scroll',()=>{
-//     if(scrollY>100){
-//         scroll.style.top="-150px";       /*HIDE*/
-//         // alert("it's working!");
-//     } 
-   
-//     /*
-//     else if(scrollY<40){
-//         scroll.style.top="0";
-//     }*/
-//     else if(scrollY<10){                /*SHOW*/
-//         scroll.style.top="0";
-//         // alert("it's working!");
-
-//     }
-    
-// })
-
-const scroll = document.querySelector('.header');
+const navigationBar = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
-  if (scrollY > 200) {
-    scroll.style.top = "-150px"; // hide
+  if (scrollY > 500) {
+    navigationBar.style.top = "-150px"; // hide
     console.log("Hello bro!");
   } 
 //   else {
 //     scroll.style.top = "0";      // show
-//   }
-    else if(scrollY < 100){
-       scroll.style.top="0";
+//    }
+    else if(scrollY < 500){
+      navigationBar.style.top="0";
     }
 
 });
